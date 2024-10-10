f55764849da305cdc419f12d82f46122fd379e3a
Quesai <isaquesantana421@gmail.com>
2024-10-10 17:25:57 -0300

    Delete laepv directory

--- laepv/public/home.js
+++
@@ -1,7 +0,0 @@
-function openContent(idTab) {
-    var content = document.getElementsByClassName('content')
-    for (var i = 0; i < content.length; i++) {
-        content[i].style.display = 'none';
-    }
-    document.getElementById(idTab).style.display = 'block';
-}
